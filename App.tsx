2bd912c5b5a63d4a6fac3f89ed3afef81224a9f8
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-07-23 23:19:03 -0300

    refactor styles

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ import {
 } from 'react-native';
 import { Canvas, Path, Skia, SkPath } from '@shopify/react-native-skia';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import { styles } from './styles';
 
 export default function App() {
   const [paths, setPaths] = useState<{ path: SkPath; color: string; stroke: number }[]>([]);
@@ -110,84 +111,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
-  },
-  canvas: {
-    flex: 1,
-    backgroundColor: 'white',
-  },
-  colorPicker: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 10,
-    backgroundColor: '#eee',
-  },
-  colorButton: {
-    width: 30,
-    height: 30,
-    borderRadius: 15,
-    marginHorizontal: 4,
-    borderWidth: 2,
-    borderColor: 'transparent',
-  },
-  selectedColor: {
-    borderColor: '#000',
-  },
-  eraserButton: {
-    width: 30,
-    height: 30,
-    borderRadius: 15,
-    marginHorizontal: 4,
-    backgroundColor: '#fff',
-    borderWidth: 2,
-    borderColor: '#000',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  selectedEraser: {
-    backgroundColor: '#ccc',
-  },
-  eraserText: {
-    fontWeight: 'bold',
-    color: '#000',
-  },
-  thicknessButton: {
-    width: 30,
-    height: 30,
-    borderRadius: 15,
-    marginHorizontal: 4,
-    backgroundColor: '#fff',
-    borderWidth: 2,
-    borderColor: '#000',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  sliderContainer: {
-    position: 'absolute',
-    bottom: 50,
-    right: 10,
-    alignItems: 'center',
-  },
-  sliderTrack: {
-    width: 40,
-    height: 150,
-    backgroundColor: '#ddd',
-    borderRadius: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingVertical: 10,
-  },
-  sliderIndicator: {
-    width: 20,
-    backgroundColor: '#333',
-    borderRadius: 10,
-  },
-  strokeLabel: {
-    marginTop: 6,
-    fontSize: 14,
-    fontWeight: 'bold',
-  },
-});
---
+++ styles.ts
@@ -0,0 +1,83 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    wrapper: {
+      flex: 1,
+    },
+    canvas: {
+      flex: 1,
+      backgroundColor: 'white',
+    },
+    colorPicker: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      padding: 10,
+      backgroundColor: '#eee',
+    },
+    colorButton: {
+      width: 30,
+      height: 30,
+      borderRadius: 15,
+      marginHorizontal: 4,
+      borderWidth: 2,
+      borderColor: 'transparent',
+    },
+    selectedColor: {
+      borderColor: '#000',
+    },
+    eraserButton: {
+      width: 30,
+      height: 30,
+      borderRadius: 15,
+      marginHorizontal: 4,
+      backgroundColor: '#fff',
+      borderWidth: 2,
+      borderColor: '#000',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    selectedEraser: {
+      backgroundColor: '#ccc',
+    },
+    eraserText: {
+      fontWeight: 'bold',
+      color: '#000',
+    },
+    thicknessButton: {
+      width: 30,
+      height: 30,
+      borderRadius: 15,
+      marginHorizontal: 4,
+      backgroundColor: '#fff',
+      borderWidth: 2,
+      borderColor: '#000',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    sliderContainer: {
+      position: 'absolute',
+      bottom: 50,
+      right: 10,
+      alignItems: 'center',
+    },
+    sliderTrack: {
+      width: 40,
+      height: 150,
+      backgroundColor: '#ddd',
+      borderRadius: 20,
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingVertical: 10,
+    },
+    sliderIndicator: {
+      width: 20,
+      backgroundColor: '#333',
+      borderRadius: 10,
+    },
+    strokeLabel: {
+      marginTop: 6,
+      fontSize: 14,
+      fontWeight: 'bold',
+    },
+  });
+  
